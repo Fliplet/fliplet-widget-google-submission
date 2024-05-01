077f6c25584d507bc81f3a6bff9566f324cca5e2
Dawid Witulski <dwitulski@fliplet.com>
2024-05-01 13:28:39 +0200

    feat(ID-4319): lint fix

--- js/gcmValidation.js
+++ js/gcmValidation.js
@@ -1,49 +1,53 @@
-const gcmServerKeyInput = document.getElementById("fl-push-serverKey");
-const gcmTestButton = document.getElementById("fl-push-testConfigButton");
+const gcmServerKeyInput = document.getElementById('fl-push-serverKey');
+const gcmTestButton = document.getElementById('fl-push-testConfigButton');
 
-const gcmTestSuccessMessage = document.getElementById("fl-push-testSuccessMessage");
-const gcmTestErrorMessage = document.getElementById("fl-push-testErrorMessage");
+const gcmTestSuccessMessage = document.getElementById('fl-push-testSuccessMessage');
+const gcmTestErrorMessage = document.getElementById('fl-push-testErrorMessage');
 
 async function validateGcmServerKey(gcmServerKey) {
   try {
     const response = await fetch(
       `https://fcm.googleapis.com/fcm/send`,
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `key=${gcmServerKey}`
         },
         body: JSON.stringify({
-          registration_ids: ["fake_device token"] // Use a non-real token
+          registration_ids: ['fake_device token'] // Use a non-real token
         })
       }
     );
 
     if (!response.ok) {
       if (response.status === 401) {
-        throw new Error("Invalid GCM Server Key");
+        throw new Error('Invalid GCM Server Key');
       }
+
       throw new Error(`Sending failed with status: ${response.status}`);
     }
+
     return true;
   } catch (error) {
-    console.error("Failed to send message:", error);
+    console.error('Failed to send message:', error);
+
     return false;
   }
 }
 
-gcmTestButton.addEventListener("click", async () => {
-  gcmTestErrorMessage.style.display = "none";
-  gcmTestSuccessMessage.style.display = "none";
+gcmTestButton.addEventListener('click', async () => {
+  gcmTestErrorMessage.style.display = 'none';
+  gcmTestSuccessMessage.style.display = 'none';
 
   const gcmServerKey = gcmServerKeyInput.value;
   const isValid = await validateGcmServerKey(gcmServerKey);
 
   if (!isValid) {
-    gcmTestErrorMessage.style.display = "block";
+    gcmTestErrorMessage.style.display = 'block';
+
     return;
   }
 
-  gcmTestSuccessMessage.style.display = "block";
+  gcmTestSuccessMessage.style.display = 'block';
 });
