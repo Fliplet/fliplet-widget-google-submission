ebef802449661e7f3dcdd2ab789f3bf1ea0d46b6
Dawid Witulski <dwitulski@fliplet.com>
2024-05-01 13:20:59 +0200

    feat(ID-4319): added testing serverKey

---
+++ js/gcmValidation.js
@@ -0,0 +1,49 @@
+const gcmServerKeyInput = document.getElementById("fl-push-serverKey");
+const gcmTestButton = document.getElementById("fl-push-testConfigButton");
+
+const gcmTestSuccessMessage = document.getElementById("fl-push-testSuccessMessage");
+const gcmTestErrorMessage = document.getElementById("fl-push-testErrorMessage");
+
+async function validateGcmServerKey(gcmServerKey) {
+  try {
+    const response = await fetch(
+      `https://fcm.googleapis.com/fcm/send`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `key=${gcmServerKey}`
+        },
+        body: JSON.stringify({
+          registration_ids: ["fake_device token"] // Use a non-real token
+        })
+      }
+    );
+
+    if (!response.ok) {
+      if (response.status === 401) {
+        throw new Error("Invalid GCM Server Key");
+      }
+      throw new Error(`Sending failed with status: ${response.status}`);
+    }
+    return true;
+  } catch (error) {
+    console.error("Failed to send message:", error);
+    return false;
+  }
+}
+
+gcmTestButton.addEventListener("click", async () => {
+  gcmTestErrorMessage.style.display = "none";
+  gcmTestSuccessMessage.style.display = "none";
+
+  const gcmServerKey = gcmServerKeyInput.value;
+  const isValid = await validateGcmServerKey(gcmServerKey);
+
+  if (!isValid) {
+    gcmTestErrorMessage.style.display = "block";
+    return;
+  }
+
+  gcmTestSuccessMessage.style.display = "block";
+});
