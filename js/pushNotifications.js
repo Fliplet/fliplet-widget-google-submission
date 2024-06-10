ce7dc3aca6229a59724b7d5ec361d2854bd8fab0
Dawid Witulski <dwitulski@fliplet.com>
2024-06-10 23:42:10 +0200

    feat(ID-4314): saving fcm config

---
+++ js/pushNotifications.js
@@ -0,0 +1,187 @@
+const saveButton = document.querySelector('.btn[data-push-save]');
+
+const fileInputs = {
+  googleServices: document.getElementById('fl-push-google-services-file'),
+  serviceAccount: document.getElementById('fl-push-service-account-file'),
+};
+
+const fileInputsValidationMessageElements = {
+  googleServices: document.getElementById('fl-push-google-services-validation'),
+  serviceAccount: document.getElementById('fl-push-service-account-validation'),
+};
+
+const uploadTimestampElements = {
+  googleServices: document.getElementById('fl-push-google-services-timestamp'),
+  serviceAccount: document.getElementById('fl-push-service-account-timestamp'),
+};
+
+const renderTimestamps = ({ googleServicesTimestamp, serviceAccountTimestamp }) => {
+  if (googleServicesTimestamp) {
+    uploadTimestampElements.googleServices.innerHTML = new Date(googleServicesTimestamp).toLocaleString();
+  }
+
+  if (serviceAccountTimestamp) {
+    uploadTimestampElements.serviceAccount.innerHTML = new Date(serviceAccountTimestamp).toLocaleString();
+  }
+
+  if (googleServicesTimestamp && serviceAccountTimestamp) {
+    saveButton.removeAttribute('disabled');
+  }
+}
+
+let previousState = {};
+
+const state = {
+  isGoogleServicesFileValid: undefined,
+  isServiceAccountFileValid: undefined,
+  formData: {},
+};
+
+// called from interface.js after fetching the widget instance data
+const onLoadPushNotificationData = (data) => {
+  previousState = structuredClone(data);
+  state.formData = data;
+  renderTimestamps(previousState);
+}
+
+const FILE_VALIDATION_MESSAGE = {
+  SUCCESS: 'File is valid',
+  ERROR: 'File is invalid',
+};
+
+const SERVICE_ACCUNT_FILE_VALIDATION_MESSAGE = {
+  INVALID_JWT: 'Error: the private_key is not associated with the project',
+  PERMISSION_DENIED: 'Error: the client does not have proper permission for the project',
+  INVALID_GRANT: 'Error: the client_email associated with the project is not found',
+  INVALID_PRIVATE_KEY: 'Error: the private_key is invalid',
+}
+
+const validateGoogleServicesFile = async (file) => {
+  const fileReader = new FileReader();
+
+  const validationMessageElement = fileInputsValidationMessageElements.googleServices;
+
+  fileReader.addEventListener('load', () => {
+    const fileContentJSON = JSON.parse(fileReader.result);
+    state.isGoogleServicesFileValid = validateGoogleServicesSchema(fileContentJSON);
+
+
+    validationMessageElement.innerHTML = state.isGoogleServicesFileValid 
+      ? FILE_VALIDATION_MESSAGE.SUCCESS
+      : FILE_VALIDATION_MESSAGE.ERROR;
+
+    validationMessageElement.classList.toggle('text-danger', !state.isGoogleServicesFileValid );
+    validationMessageElement.classList.toggle('text-success', state.isGoogleServicesFileValid );
+
+    if (state.isGoogleServicesFileValid) state.formData.googleServices = fileContentJSON;
+
+    if (!state.isGoogleServicesFileValid || state.isServiceAccountFileValid === false) saveButton.setAttribute('disabled', true);
+    else saveButton.removeAttribute('disabled');
+  });
+
+  fileReader.readAsText(file);
+};
+
+fileInputs.googleServices.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  if (!file) return;
+  validateGoogleServicesFile(file);
+});
+
+const validateServiceAccountFile = async (file) => {
+  const fileReader = new FileReader();
+
+  const validationMessageElement = fileInputsValidationMessageElements.serviceAccount;
+
+  fileReader.addEventListener('load', async () => {
+    const fileContentJSON = JSON.parse(fileReader.result);
+    const { client_email, project_id, private_key } = fileContentJSON;
+
+    if (!client_email || !project_id || !private_key) {
+      state.isServiceAccountFileValid = false;
+      validationMessageElement.innerHTML = FILE_VALIDATION_MESSAGE.ERROR;
+      validationMessageElement.classList.toggle('text-danger', !state.isServiceAccountFileValid );
+      saveButton.setAttribute('disabled', true);
+
+      return;
+    }
+
+    const response = await fetch(`/v1/apps/${Fliplet.Env.get('appId')}/notifications/validate-fcm-config`, { method: 'POST', body: JSON.stringify({ client_email, project_id, private_key} )});
+    const { message } = await response.json();
+
+    let errorMessage = null;
+
+    if (message.includes('Permission denied on resource project')) {
+      errorMessage = SERVICE_ACCUNT_FILE_VALIDATION_MESSAGE.PERMISSION_DENIED;
+    }
+
+    if (message.includes('Invalid JWT')) {
+      errorMessage = SERVICE_ACCUNT_FILE_VALIDATION_MESSAGE.INVALID_JWT;
+    }
+
+    if (message.includes('Invalid grant')) {
+      errorMessage = SERVICE_ACCUNT_FILE_VALIDATION_MESSAGE.INVALID_GRANT;
+    }
+
+    if (message.includes('PEM routines:')) {
+      errorMessage = SERVICE_ACCUNT_FILE_VALIDATION_MESSAGE.INVALID_PRIVATE_KEY;
+    }
+
+    state.isServiceAccountFileValid = !errorMessage && message === 'SenderId mismatch';
+
+    validationMessageElement.innerHTML = state.isServiceAccountFileValid
+      ? FILE_VALIDATION_MESSAGE.SUCCESS
+      : errorMessage || FILE_VALIDATION_MESSAGE.ERROR;
+
+    validationMessageElement.classList.toggle('text-danger', !state.isServiceAccountFileValid );
+    validationMessageElement.classList.toggle('text-success', state.isServiceAccountFileValid );
+
+    if (state.isServiceAccountFileValid) {
+      state.formData = {
+        ...state.formData,
+        client_email,
+        project_id,
+        private_key
+      }
+    }
+
+    if (!state.isServiceAccountFileValid || state.isGoogleServicesFileValid === false) saveButton.setAttribute('disabled', true);
+    else saveButton.removeAttribute('disabled');
+  });
+
+  fileReader.readAsText(file);
+}
+
+fileInputs.serviceAccount.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  if (!file) return;
+  validateServiceAccountFile(file);
+});
+
+const savePushData = async () => {
+  // TODO: get rid of lodash here
+  const googleServicesChanged = !_.isEqual(state.formData.googleServices, previousState.googleServices);
+  const serviceAccountChanged = state.formData.client_email !== previousState.client_email || state.formData.project_id !== previousState.project_id || state.formData.private_key !== previousState.private_key;
+
+  if (!googleServicesChanged && !serviceAccountChanged) {
+    return;
+  }
+
+  if (googleServicesChanged || !previousState.googleServicesTimestamp) {
+    state.formData.googleServicesTimestamp = new Date().getTime();
+  }
+  
+  if (serviceAccountChanged || !previousState.serviceAccountTimestamp) {
+    state.formData.serviceAccountTimestamp = new Date().getTime();
+  }
+
+  await Fliplet.API.request({
+    method: 'PUT',
+    url: 'v1/widget-instances/com.fliplet.push-notifications?appId=' + Fliplet.Env.get('appId'),
+    data: state.formData,
+  });
+
+  renderTimestamps(state.formData);
+}
+
+saveButton.addEventListener('click', savePushData);
--- js/schemaValidation.js
+++ js/schemaValidation.js
@@ -1,81 +1,84 @@
 // This is simple schema validation for the input data
 
 const googleServicesSchema = {
-  $schema: "http://json-schema.org/draft-07/schema#",
-  type: "object",
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  type: 'object',
   properties: {
     project_info: {
-      type: "object",
+      type: 'object',
       properties: {
-        project_number: { type: "string" },
-        project_id: { type: "string" },
-        storage_bucket: { type: "string" },
+        project_number: { type: 'string' },
+        project_id: { type: 'string' },
+        storage_bucket: { type: 'string' }
       },
-      required: ["project_number", "project_id", "storage_bucket"],
+      required: ['project_number', 'project_id', 'storage_bucket']
     },
     client: {
-      type: "array",
+      type: 'array',
       items: {
-        type: "object",
+        type: 'object',
         properties: {
           client_info: {
-            type: "object",
+            type: 'object',
             properties: {
-              mobilesdk_app_id: { type: "string" },
+              mobilesdk_app_id: { type: 'string' },
               android_client_info: {
-                type: "object",
+                type: 'object',
                 properties: {
-                  package_name: { type: "string" },
+                  package_name: { type: 'string' }
                 },
-                required: ["package_name"],
-              },
+                required: ['package_name']
+              }
             },
-            required: ["mobilesdk_app_id", "android_client_info"],
+            required: ['mobilesdk_app_id', 'android_client_info']
           },
           oauth_client: {
-            type: "array",
-            items: { type: "object" },
+            type: 'array',
+            items: { type: 'object' }
           },
           api_key: {
-            type: "array",
+            type: 'array',
             items: {
-              type: "object",
+              type: 'object',
               properties: {
-                current_key: { type: "string" },
+                current_key: { type: 'string' }
               },
-              required: ["current_key"],
-            },
+              required: ['current_key']
+            }
           },
           services: {
-            type: "object",
+            type: 'object',
             properties: {
               appinvite_service: {
-                type: "object",
+                type: 'object',
                 properties: {
                   other_platform_oauth_client: {
-                    type: "array",
-                    items: { type: "object" },
-                  },
+                    type: 'array',
+                    items: { type: 'object' }
+                  }
                 },
-                required: ["other_platform_oauth_client"],
-              },
+                required: ['other_platform_oauth_client']
+              }
             },
-            required: ["appinvite_service"],
-          },
+            required: ['appinvite_service']
+          }
         },
-        required: ["client_info", "oauth_client", "api_key", "services"],
-      },
+        required: ['client_info', 'oauth_client', 'api_key', 'services']
+      }
     },
-    configuration_version: { type: "string" },
+    configuration_version: { type: 'string' }
   },
-  required: ["project_info", "client", "configuration_version"],
+  required: ['project_info', 'client', 'configuration_version']
 };
 
 const validateSchema = (schema, data) => {
   const validateType = (value, type) => {
-    if (type === "array") return Array.isArray(value);
-    if (type === "object")
-      return value && typeof value === "object" && !Array.isArray(value);
+    if (type === 'array') return Array.isArray(value);
+
+    if (type === 'object') {
+      return value && typeof value === 'object' && !Array.isArray(value);
+    }
+
     return typeof value === type;
   };
 
@@ -89,16 +92,19 @@ const validateSchema = (schema, data) => {
     for (const key in data) {
       if (schema.properties[key]) {
         if (!validateType(data[key], schema.properties[key].type)) return false;
-        if (schema.properties[key].type === "object") {
+
+        if (schema.properties[key].type === 'object') {
           if (!validateObject(schema.properties[key], data[key])) return false;
         }
-        if (schema.properties[key].type === "array") {
+
+        if (schema.properties[key].type === 'array') {
           if (
             !data[key].every((item) =>
               validateObject(schema.properties[key].items, item)
             )
-          )
+          ) {
             return false;
+          }
         }
       }
     }
