4a707e519ff0137a6d109b0a6b6b7dcb2236ef31
Inna Bieshulia <46998323+inna-bieshulia@users.noreply.github.com>
2024-06-11 18:50:20 +0300

    Revert "ID-4314 [feat] GCM to FCM migration (#93)"
    This reverts commit da0c0b8cb8a06465704ef909b9c86e355ed56876.

--- js/interface.js
+++ js/interface.js
@@ -14,6 +14,7 @@ var enterpriseFirebaseFileField;
 var previousAppStoreSubmission = {};
 var enterpriseSubmission = {};
 var previousEnterpriseSubmission = {};
+var notificationSettings = {};
 var appInfo;
 var statusTableTemplate = $('#status-table-template').html();
 var $statusAppStoreTableElement = $('.app-build-appstore-status-holder');
@@ -24,6 +25,11 @@ var hasFolders = false;
 var screenShotsMobile = [];
 var screenShotsTablet = [];
 var haveScreenshots = false;
+var pushDataMap = {
+  'fl-push-senderId': 'gcmSenderId',
+  'fl-push-serverKey': 'gcmServerKey',
+  'fl-push-projectId': 'gcmProjectId'
+};
 var formInputSelectors = [
   '#appStoreConfiguration :input',
   '#enterpriseConfiguration :input',
@@ -51,6 +57,38 @@ function incrementVersionNumber(versionNumber) {
   }
 
   return splitNumber.join('.');
+}
+
+function saveFirebaseSettings(origin) {
+  var formData;
+
+  if (origin === 'appStore' && appStoreFirebaseFileField && appStoreFirebaseFileField.files[0]) {
+    formData = new FormData();
+
+    formData.append('firebase', appStoreFirebaseFileField.files[0]);
+
+    return setFirebaseConfigFile(appStoreSubmission.id, formData);
+  }
+
+  if (origin === 'enterprise' && enterpriseFirebaseFileField && enterpriseFirebaseFileField.files[0]) {
+    formData = new FormData();
+
+    formData.append('firebase', enterpriseFirebaseFileField.files[0]);
+
+    return setFirebaseConfigFile(enterpriseSubmission.id, formData);
+  }
+
+  return Promise.resolve();
+}
+
+function setFirebaseConfigFile(id, file) {
+  return Fliplet.API.request({
+    method: 'PUT',
+    url: 'v1/organizations/' + Fliplet.Env.get('organizationId') + '/credentials/submission-' + id + '?fileName=firebase',
+    data: file,
+    contentType: false,
+    processData: false
+  });
 }
 
 function incrementVersionCode(versionNumber) {
@@ -312,11 +350,26 @@ function loadEnterpriseData() {
   }
 }
 
+function loadPushNotesData() {
+  $('#pushConfiguration [name]').each(function(i, el) {
+    var name = $(el).attr('name');
+
+    if (!pushDataMap.hasOwnProperty(name)) {
+      return;
+    }
+
+    /* ADDING NOTIFICATIONS SETTINGS */
+    $(this).val(notificationSettings[pushDataMap[name]] || '');
+  });
+}
+
 function submissionBuild(appSubmission, origin) {
   var newVersionNumber;
   var newVersionCode;
 
-  Fliplet.App.Submissions.build(appSubmission.id).then(function(builtSubmission) {
+  saveFirebaseSettings(origin).then(function() {
+    return Fliplet.App.Submissions.build(appSubmission.id);
+  }).then(function(builtSubmission) {
     if (origin === 'appStore') {
       appStoreSubmission = builtSubmission.submission;
 
@@ -501,6 +554,7 @@ function requestBuild(origin, submission) {
 
 function saveAppStoreData(request) {
   var data = appStoreSubmission.data || {};
+  var pushData = notificationSettings;
   var uploadFilePromise = Promise.resolve();
 
   $('#appStoreConfiguration [name]').each(function(idx, el) {
@@ -512,6 +566,7 @@ function saveAppStoreData(request) {
     }
 
     if (name === 'fl-store-bundleId') {
+      pushData.gcmPackageName = value;
       data[name] = value;
 
       return;
@@ -548,6 +603,9 @@ function saveAppStoreData(request) {
 
   return uploadFilePromise.then(function() {
     appStoreSubmission.data = data;
+    notificationSettings = pushData;
+
+    savePushData(true);
 
     if (request) {
       if (!storeFeatures.public) {
@@ -622,6 +680,8 @@ function saveEnterpriseData(request) {
   return uploadFilePromise.then(function() {
     enterpriseSubmission.data = data;
 
+    savePushData(true);
+
     if (request) {
       if (!storeFeatures.private) {
         Fliplet.Studio.emit('overlay', {
@@ -648,6 +708,48 @@ function saveEnterpriseData(request) {
     }
 
     return save('enterprise', enterpriseSubmission);
+  });
+}
+
+function savePushData(silentSave) {
+  var data = notificationSettings || {};
+
+  $('#pushConfiguration [name]').each(function(i, el) {
+    var name = $(el).attr('name');
+
+    if (!pushDataMap.hasOwnProperty(name)) {
+      return;
+    }
+
+    var value = $(el).val();
+
+    if (typeof value === 'string') {
+      value = value.trim();
+    }
+
+    data[pushDataMap[name]] = value;
+  });
+
+  data.gcm = !!((data.gcmSenderId && data.gcmSenderId !== '') && (data.gcmServerKey && data.gcmServerKey !== ''));
+
+  notificationSettings = data;
+
+  Fliplet.API.request({
+    method: 'PUT',
+    url: 'v1/widget-instances/com.fliplet.push-notifications?appId=' + Fliplet.Env.get('appId'),
+    data: notificationSettings
+  }).then(function() {
+    $('.save-push-progress').addClass('saved');
+
+    if (!silentSave && (typeof appStoreSubmission.data['fl-store-bundleId'] === 'undefined' || typeof enterpriseSubmission.data['fl-ent-bundleId'] === 'undefined')) {
+      Fliplet.Modal.alert({
+        message: 'For notifications to work, you will need to fill in the Bundle ID field and request an app.'
+      });
+    }
+
+    setTimeout(function() {
+      $('.save-push-progress').removeClass('saved');
+    }, 4000);
   });
 }
 
@@ -688,6 +790,7 @@ function init() {
 
   loadAppStoreData();
   loadEnterpriseData();
+  loadPushNotesData();
   Fliplet.Widget.autosize();
 }
 
@@ -1167,6 +1270,9 @@ $('[data-app-store-save]').on('click', function() {
 $('[data-enterprise-save]').on('click', function() {
   saveEnterpriseData();
 });
+$('[data-push-save]').on('click', function() {
+  savePushData();
+});
 
 // Scroll accordion tab to the top
 $('.panel-collapse').on('shown.bs.collapse', function() {
@@ -1613,9 +1719,11 @@ function initialLoad(initial, timeout) {
         });
       })
       .then(function(response) {
-        const notificationSettings = response.widgetInstance.settings && response.widgetInstance.settings ? response.widgetInstance.settings : {};
-
-        onLoadPushNotificationData(notificationSettings);
+        if (response.widgetInstance.settings && response.widgetInstance.settings) {
+          notificationSettings = response.widgetInstance.settings;
+        } else {
+          notificationSettings = {};
+        }
 
         init();
         initialLoad(false, 5000);
