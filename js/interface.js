3d983805db3cd569cf8e8f470c8fab9d3020f873
dwfliplet <144125903+dwfliplet@users.noreply.github.com>
2024-06-11 10:40:47 +0200

    ID-4314 [feat] GCM to FCM migration (#87)
    * feat(ID-4314): added fcm form; added google services json schema validation
    
    * feat(ID-4314): added services account validation
    
    * feat(ID-4314): saving fcm config
    
    * feat(ID-4314): changed learn more about analytics link
    
    * feat(ID-4314): added fcm and gcm flags to widget settings

--- js/interface.js
+++ js/interface.js
@@ -14,7 +14,6 @@ var enterpriseFirebaseFileField;
 var previousAppStoreSubmission = {};
 var enterpriseSubmission = {};
 var previousEnterpriseSubmission = {};
-var notificationSettings = {};
 var appInfo;
 var statusTableTemplate = $('#status-table-template').html();
 var $statusAppStoreTableElement = $('.app-build-appstore-status-holder');
@@ -25,11 +24,6 @@ var hasFolders = false;
 var screenShotsMobile = [];
 var screenShotsTablet = [];
 var haveScreenshots = false;
-var pushDataMap = {
-  'fl-push-senderId': 'gcmSenderId',
-  'fl-push-serverKey': 'gcmServerKey',
-  'fl-push-projectId': 'gcmProjectId'
-};
 var formInputSelectors = [
   '#appStoreConfiguration :input',
   '#enterpriseConfiguration :input',
@@ -350,19 +344,6 @@ function loadEnterpriseData() {
   }
 }
 
-function loadPushNotesData() {
-  $('#pushConfiguration [name]').each(function(i, el) {
-    var name = $(el).attr('name');
-
-    if (!pushDataMap.hasOwnProperty(name)) {
-      return;
-    }
-
-    /* ADDING NOTIFICATIONS SETTINGS */
-    $(this).val(notificationSettings[pushDataMap[name]] || '');
-  });
-}
-
 function submissionBuild(appSubmission, origin) {
   var newVersionNumber;
   var newVersionCode;
@@ -554,7 +535,6 @@ function requestBuild(origin, submission) {
 
 function saveAppStoreData(request) {
   var data = appStoreSubmission.data || {};
-  var pushData = notificationSettings;
   var uploadFilePromise = Promise.resolve();
 
   $('#appStoreConfiguration [name]').each(function(idx, el) {
@@ -566,7 +546,6 @@ function saveAppStoreData(request) {
     }
 
     if (name === 'fl-store-bundleId') {
-      pushData.gcmPackageName = value;
       data[name] = value;
 
       return;
@@ -603,9 +582,6 @@ function saveAppStoreData(request) {
 
   return uploadFilePromise.then(function() {
     appStoreSubmission.data = data;
-    notificationSettings = pushData;
-
-    savePushData(true);
 
     if (request) {
       if (!storeFeatures.public) {
@@ -680,8 +656,6 @@ function saveEnterpriseData(request) {
   return uploadFilePromise.then(function() {
     enterpriseSubmission.data = data;
 
-    savePushData(true);
-
     if (request) {
       if (!storeFeatures.private) {
         Fliplet.Studio.emit('overlay', {
@@ -708,48 +682,6 @@ function saveEnterpriseData(request) {
     }
 
     return save('enterprise', enterpriseSubmission);
-  });
-}
-
-function savePushData(silentSave) {
-  var data = notificationSettings || {};
-
-  $('#pushConfiguration [name]').each(function(i, el) {
-    var name = $(el).attr('name');
-
-    if (!pushDataMap.hasOwnProperty(name)) {
-      return;
-    }
-
-    var value = $(el).val();
-
-    if (typeof value === 'string') {
-      value = value.trim();
-    }
-
-    data[pushDataMap[name]] = value;
-  });
-
-  data.gcm = !!((data.gcmSenderId && data.gcmSenderId !== '') && (data.gcmServerKey && data.gcmServerKey !== ''));
-
-  notificationSettings = data;
-
-  Fliplet.API.request({
-    method: 'PUT',
-    url: 'v1/widget-instances/com.fliplet.push-notifications?appId=' + Fliplet.Env.get('appId'),
-    data: notificationSettings
-  }).then(function() {
-    $('.save-push-progress').addClass('saved');
-
-    if (!silentSave && (typeof appStoreSubmission.data['fl-store-bundleId'] === 'undefined' || typeof enterpriseSubmission.data['fl-ent-bundleId'] === 'undefined')) {
-      Fliplet.Modal.alert({
-        message: 'For notifications to work, you will need to fill in the Bundle ID field and request an app.'
-      });
-    }
-
-    setTimeout(function() {
-      $('.save-push-progress').removeClass('saved');
-    }, 4000);
   });
 }
 
@@ -790,7 +722,6 @@ function init() {
 
   loadAppStoreData();
   loadEnterpriseData();
-  loadPushNotesData();
   Fliplet.Widget.autosize();
 }
 
@@ -1270,9 +1201,6 @@ $('[data-app-store-save]').on('click', function() {
 $('[data-enterprise-save]').on('click', function() {
   saveEnterpriseData();
 });
-$('[data-push-save]').on('click', function() {
-  savePushData();
-});
 
 // Scroll accordion tab to the top
 $('.panel-collapse').on('shown.bs.collapse', function() {
@@ -1719,11 +1647,9 @@ function initialLoad(initial, timeout) {
         });
       })
       .then(function(response) {
-        if (response.widgetInstance.settings && response.widgetInstance.settings) {
-          notificationSettings = response.widgetInstance.settings;
-        } else {
-          notificationSettings = {};
-        }
+        const notificationSettings = response.widgetInstance.settings && response.widgetInstance.settings ? response.widgetInstance.settings : {};
+
+        onLoadPushNotificationData(notificationSettings);
 
         init();
         initialLoad(false, 5000);
